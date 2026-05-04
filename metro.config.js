78ca7f16c6d1dd0ef21173a67d695971a0ef57d1
reminders-tool <dev@reminders-tool>
2026-05-04 14:15:24 +0000

    fix: properly resolve @ path alias in metro.config.js using extraNodeModules proxy

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,20 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.alias = {
-  '@': path.resolve(__dirname),
-};
+const projectRoot = __dirname;
+
+config.resolver.extraNodeModules = new Proxy(
+  {},
+  {
+    get: (target, name) => {
+      if (name === '@') {
+        return projectRoot;
+      }
+      return path.join(projectRoot, 'node_modules', name);
+    },
+  }
+);
+
+config.watchFolders = [projectRoot];
 
 module.exports = config;
